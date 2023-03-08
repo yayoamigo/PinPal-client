2f7ca15bf10b658596cf264aaa7c0ebb53dd245c
david zambrano corral <Yayoamigo>
2023-03-08 16:41:06 -0500

    Fix build

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,7 +19,7 @@ export default function Register({ setShowRegister }) {
     };
 
     try {
-      await axios.post("/users/register", newUser);
+      await axios.post("https://54.235.5.95:8000/api/users/register", newUser);
       setError(false);
       setSuccess(true);
     } catch (err) {
